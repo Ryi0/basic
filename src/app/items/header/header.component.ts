d701c4a3f5664ca51e5665e8e5cfc5155ca2bdb4
ryi0 <wpaquette455@gmail.com>
2024-03-11 00:48:37 -0400

    0.0.3  added burger

--- src/app/items/header/header.component.ts
+++ src/app/items/header/header.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {NgIf} from "@angular/common";
+import {ButtonComponent} from "../button/button.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    NgIf,
+    ButtonComponent
   ],
   template: `
 
     <header>
-      <app-navbar></app-navbar>
+      <app-navbar *ngIf="showNavbar()"></app-navbar>
+      <button (click)="toggleNavbar()">
+        <span *ngIf="!showNavbar()">&#9776;</span>
+        <span *ngIf="showNavbar()">&#10005;</span>
+      </button>
 
     </header>
 
@@ -18,5 +26,9 @@ import {NavbarComponent} from "../navbar/navbar.component";
   styles: ``
 })
 export class HeaderComponent {
+  showNavbar= signal(false);
 
+  toggleNavbar() {
+    this.showNavbar.update(value => !value) ;
+  }
 }
